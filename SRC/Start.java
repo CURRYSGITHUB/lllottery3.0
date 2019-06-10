ad1016e74fb250af78d0da084991ffbb1ffbfcdf
DamienV11 <51413983+DamienV11@users.noreply.github.com>
2019-06-10 17:41:50 +0800

    Update and rename src/Start.java to SRC/Start.java

--- SRC/Start.java
+++ SRC/Start.java
@@ -4,7 +4,9 @@ import	java.awt.event.*;
 
 public class Start {
 	static JFrame frame;
+	static ImageIcon bgImage = new ImageIcon("C:/Users/Damien/Desktop/bg.jpg");
 	static JLabel label;
+	static JLabel bgLabel = new JLabel(bgImage);
 	static JButton b1,b2,b3;
 	public static void cjStart () {
 		frame = new JFrame();//创建frame
@@ -12,7 +14,7 @@ public class Start {
 		label = new JLabel("Java 抽 奖 系 统");
 		Font font1 = new Font("微软雅黑",Font.BOLD,44);
 		label.setFont(font1);//设置label字体大小
-		label.setBounds(300,80,350,100);
+		label.setBounds(300,90,350,100);
 		b1 = new JButton("用 户 登 录");
 		b2 = new JButton("用 户 注 册");
 		b3 = new JButton("管 理 员 登 录");
@@ -25,16 +27,24 @@ public class Start {
 		b1.setFont(font2);
 		b2.setFont(font2);
 		b3.setFont(font2);
-		b1.setBounds(350, 240, 197, 52);
-		b2.setBounds(350, 331, 197, 52);
-        b3.setBounds(350, 422, 197, 52);
+		b1.setBounds(360, 270, 197, 52);
+		b2.setBounds(360, 360, 197, 52);
+        b3.setBounds(360, 450, 197, 52);
 
-		
-		frame.setBounds(100, 100, 1000, 800);
+		frame.setSize(950, 750);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);//将布局管理器置为空
         frame.setVisible(true);
         
+        bgLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+		// 把内容窗格转化为JPanel，否则不能用方法setOpaque()来使内容窗格透明
+		JPanel imagePanel = (JPanel) frame.getContentPane();
+		imagePanel.setOpaque(false);
+		// 把背景图片添加到分层窗格的最底层作为背景
+		frame.getLayeredPane().add(bgLabel, new Integer(Integer.MIN_VALUE));
+		frame.setResizable(false);
+		
         frame.getContentPane().add(label);
         frame.getContentPane().add(b1);
         frame.getContentPane().add(b2);
