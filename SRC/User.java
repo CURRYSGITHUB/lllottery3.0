5e658f6a7db68a06b95f8ee5cf6aeb8339718252
DamienV11 <51413983+DamienV11@users.noreply.github.com>
2019-06-10 17:40:45 +0800

    Update and rename src/User.java to SRC/User.java

--- SRC/User.java
+++ SRC/User.java
@@ -4,41 +4,55 @@ import	java.awt.*;
 import	java.awt.event.*;
 
 public class User extends Person {
-	public static int number = 0;
-	public static boolean isWin = false;
+	public static int ftimes = 0;
+	public static int stimes = 0;
+	public static int ttimes = 0;
 	public static int b;
 	
 	// JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-    static final String DB_URL = "jdbc:mysql://localhost:3306/test1?serverTimezone=GMT%2B8";//url指向要访问的数据库名
+    static final String DB_URL = "jdbc:mysql://localhost:3306/world?serverTimezone=GMT%2B8";//url指向要访问的数据库名
  
     // MySQL配置时的用户名与密码
-    static final String USER = "11";
-    static final String PASS = "12345678";
+    static final String USER = "root";
+    static final String PASS = "1315202benny";
     
 	static JFrame frame,frame2;
 	static JButton b1,b2,b3;
+	
+	static ImageIcon bgImage = new ImageIcon("C:/Users/Damien/Desktop/bg.jpg");
+	static JLabel bgLabel = new JLabel(bgImage);
+	
 	public static void swingCJ() {
 		frame = new JFrame();
-		b1 = new JButton("开 始 抽 奖");
-		b2 = new JButton("查 看 中 奖 信 息");
-		b3 = new JButton("退 出 登 录");
-		
-		frame.setBounds(100, 100, 1000, 800);
+		frame.setSize(950, 750);
+		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);//将布局管理器置为空
         frame.setVisible(true);
         
-        b1.setBounds(350, 240, 220, 52);
+        bgLabel.setBounds(0, 0, frame.getWidth(), frame.getHeight());
+		// 把内容窗格转化为JPanel，否则不能用方法setOpaque()来使内容窗格透明
+		JPanel imagePanel = (JPanel) frame.getContentPane();
+		imagePanel.setOpaque(false);
+		// 把背景图片添加到分层窗格的最底层作为背景
+		frame.getLayeredPane().add(bgLabel, new Integer(Integer.MIN_VALUE));
+		frame.setResizable(false);
+		
+		b1 = new JButton("开 始 抽 奖");
+		b2 = new JButton("查 看 中 奖 信 息");
+		b3 = new JButton("退 出 登 录");
+        
+        b1.setBounds(360, 270, 197, 50);
         b1.setBackground(Color.ORANGE);
         b1.addActionListener(new B1());
-        b2.setBounds(350, 331, 220, 52);
+        b2.setBounds(360, 360, 197, 50);
         b2.setBackground(Color.ORANGE);
         b2.addActionListener(new B2());
-        b3.setBounds(350, 422, 220, 52);
+        b3.setBounds(360, 450, 197, 50);
         b3.setBackground(Color.ORANGE);
         b3.addActionListener(new B3());
-        Font font1 = new Font("serif",Font.BOLD,24);
+        Font font1 = new Font("serif",Font.BOLD,20);
 		b1.setFont(font1);
 		b2.setFont(font1);
 		b3.setFont(font1);
@@ -50,46 +64,25 @@ public class User extends Person {
 	}
 	static class B1 implements ActionListener{
 		public void actionPerformed(ActionEvent event) {
-			Play.userCJ();
+			Play play = new Play();
+			frame.setVisible(false);
+			play.userCJ();
 		}
 	}  
 	//中奖信息查询
 	static class B2 implements ActionListener{
 		public void actionPerformed(ActionEvent event) {
-
-			frame2 = new JFrame();
-			frame2.setBounds(100, 100, 1000, 800);
-			frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame2.getContentPane().setLayout(null);//将布局管理器置为空
-	        User.frame.setVisible(false);
-	        frame2.setVisible(true);
-	        JLabel CJinfo;
-	        JButton out = new JButton("返回");
-	        Font font2 = new Font("serif",Font.BOLD,24);
-	        out.setFont(font2);
-	        out.setBounds(350, 600, 232, 57);
-	        out.setBackground(Color.ORANGE);
-	        out.addActionListener(new Out());
-	        frame2.getContentPane().add(out);
-	        Font font3 = new Font("微软雅黑",Font.BOLD,48);
-
-		   if(User.isWin) {
-			   CJinfo = new JLabel("恭喜您,您已中奖！");
-			   CJinfo.setBounds(260,250,500,200);
-			   
-			   CJinfo.setFont(font3);
-			   frame2.getContentPane().add(CJinfo);
-		   }else {
-			   CJinfo = new JLabel("很遗憾,您未中奖！");
-			   CJinfo.setBounds(260,250,500,200);
-			   CJinfo.setFont(font3);
-			   frame2.getContentPane().add(CJinfo);
-		   }
-		        
-				    
+			if(ftimes==0&&stimes==0&&ttimes==0) {
+				JOptionPane.showMessageDialog(null, "很遗憾，您没有中任何奖项");
+				
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "您的中奖情况如下"+"\n一等奖："+ftimes+"次"+"\n二等奖："+stimes+"次"+"\n三等奖："+ttimes+"次");
+			}     
 		}
+	}
 			
-	}  
+	 
 	//退出登录
 	static class B3 implements ActionListener{
 		public void actionPerformed(ActionEvent event) {
